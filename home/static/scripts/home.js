dc3e2e7374d270d3ec04e30eab4b863102ad8211
minlaxz <minminlaxz@gmail.com>
2021-01-12 01:10:52 +0630

    Home app static files
    it is working kwi kwi

--- home/static/scripts/home.js
+++ home/static/scripts/home.js
@@ -1,1 +1,13 @@
 console.log('home.js is loaded.')
+
+document.getElementById('blog').addEventListener('click', blog )
+
+document.getElementById('about').addEventListener('click', about )
+
+function blog() {
+    alert("Blog is under construct.")
+}
+
+function about() {
+    window.location.href = 'about'
+}
